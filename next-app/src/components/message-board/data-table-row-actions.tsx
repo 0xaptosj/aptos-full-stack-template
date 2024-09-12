094416a24619b8044812f2cad22ce538212c5329
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-09-12 16:58:39 -0700

    fix next build

--- next-app/src/components/message-board/data-table-row-actions.tsx
+++ next-app/src/components/message-board/data-table-row-actions.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MessageBoardColumns } from "@/lib/type/Message";
+import { MessageBoardColumns } from "@/lib/type/message";
 
 interface DataTableRowActionsProps {
   row: Row<MessageBoardColumns>;
